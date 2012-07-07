4275d97dd014ce7d71f2b1dfa198dccee9de175c
Daniel Tull <dt@danieltull.co.uk>
2012-07-07 12:23:40 +0100

    Use iOS 4 method

--- DCTImageSizing/DCTImageSizing.m
+++ DCTImageSizing/DCTImageSizing.m
@@ -12,27 +12,16 @@
 @implementation UIImage (DCTImageSizing)
 
 - (UIImage *)dct_imageWithSize:(CGSize)size contentMode:(UIViewContentMode)contentMode {
-	
-	CGImageRef imageRef = self.CGImage;
-	
-	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-	CGContextRef context = CGBitmapContextCreate(NULL, size.width, size.height, 8, 0, colorSpace, kCGImageAlphaNoneSkipLast);
-
-	CGRect rect = CGRectMake(0.0f, 0.0f, size.width, size.height);
-	CGContextSetFillColor(context, CGColorGetComponents([UIColor whiteColor].CGColor));
-	CGContextFillRect(context, rect);
 
 	CGRect imageRect = [DCTContentSizer rectForOriginalSize:self.size desiredSize:size contentMode:contentMode];
-	CGContextDrawImage(context, imageRect, imageRef);
+	UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
+	CGImageRef sourceImg = CGImageCreateWithImageInRect([self CGImage], imageRect);
+	[self drawInRect:imageRect];
+	CGImageRelease(sourceImg);
+	UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
 
-	CGImageRef scaledImageRef = CGBitmapContextCreateImage(context);
-	UIImage *scaledImage = [UIImage imageWithCGImage:scaledImageRef];
-
-	CGColorSpaceRelease(colorSpace);
-	CGContextRelease(context);
-	CGImageRelease(scaledImageRef);
-
-	return scaledImage;
+	return image;
 }
 
 @end
